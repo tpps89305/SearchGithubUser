e7f99a5dc35e88b58b42e461578d33e72b10a930
Dispy-Desktop <tpps89305@hotmail.com>
2021-05-28 15:52:06 +0800

    Initial commit

---
+++ app/src/main/java/com/dispy/searchgithubusers/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.dispy.searchgithubusers
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
